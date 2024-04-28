d045d683975637101559c8c74529bf1f32d01946
mahmoudcod <mahmoud.pk@icloud.com>
2024-04-29 02:26:28 +0300

    change the config to add the css

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
     reactStrictMode: true
 }
+
+const withCSS = require('@next/css');
+
+module.exports = withCSS({
+    cssLoaderOptions: {
+        // Options here will be passed to css-loader
+        url: false, // Disable URL handling by css-loader to prevent it from rewriting URLs
+    },
+});
