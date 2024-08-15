25bc98c0577733dad3a7067d2e65474ee96eaa45
mahmoud kamel <spop66009@gmail.com>
2024-08-15 14:16:34 +0300

    add the twiter meta tags

--- src/component/layout.js
+++ src/component/layout.js
@@ -7,10 +7,20 @@ const Layout = ({ children, title, description, image, favicon }) => (
         <Head>
             <title>{title}</title>
             <meta name="description" content={description} />
+
+            {/* Open Graph Meta Tags */}
             <meta property="og:title" content={title} />
             <meta property="og:description" content={description} />
             <meta property="og:image" content={image} />
-            <link rel="icon" href={favicon} />  // Add this line
+
+            {/* Twitter Meta Tags */}
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:title" content={title} />
+            <meta name="twitter:description" content={description} />
+            <meta name="twitter:image" content={image} />
+
+            {/* Favicon */}
+            <link rel="icon" href={favicon} />
         </Head>
         {children}
     </>
